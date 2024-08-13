1138d59dcccd07082eae95ace13e1e34563dddfc
abadredd <49213550+abadredd@users.noreply.github.com>
2024-08-12 23:44:20 -0400

    Update graphnode.cpp

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -34,11 +34,10 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> &&edge)
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot&& chatbot)
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
     _chatBot = std::move(chatbot); // Take ownership using move semantics
     _chatBot.SetCurrentNode(this);
-
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
